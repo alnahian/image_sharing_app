7b5168df5607f72de13a7c1fe1c48264a4eb5fd9
Al Nahian <nahian33@yahoo.com>
2014-06-13 13:20:46 +0600

    config file image created

---
+++ app/config/image.php
@@ -0,0 +1,14 @@
+<?php
+
+// app/config/image.php
+
+return array(
+	// folder holding original images
+	'upload_folder' => 'uploads',
+	// folder holding thumbnails
+	'thumb_folder' => 'uploads/thumbs',
+	// width of thumbnail
+	'thumb_width' => 320,
+	// height of thumbnail
+	'thumb_height' => 240
+);
